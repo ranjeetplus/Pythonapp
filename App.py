67c0ea0b3ef305da401690d4569a37d846caf4a3
ranjeetplus <ranjeetplus@gmail.com>
2025-02-28 01:14:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,8 +4,9 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-st.title("Python Application - Data Analysis Project [Pandas, Mathplotlib, GeoPandas & SeaBorn]")
-
+st.title("Python Application - Data Analysis Project")
+st.write("_______________________________________________________________________________________")
+st.write("Data Analysis Project [Pandas, Mathplotlib, GeoPandas & SeaBorn]")
 
 # Title of the Streamlit App
 st.write("State-wise TB Cases in India")
